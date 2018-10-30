376d8f18c8fd2a8792f0ff13d64d2668b338d67c
sierra <sierra@sierras-mbp-2.wireless.newschool.edu>
2018-10-30 11:12:02 -0400

    edits

--- labs/lab 5/assets/js/app.js
+++ labs/lab 5/assets/js/app.js
@@ -2,5 +2,12 @@ var name = "Sierra";
 var age = 19;
 can_drive = true;
 
-var dresser = ['socks', 'shirts', 'pants', 'old stuff'];
+var dresser = [['socks'25,] 'shirts', 'pants', 'old stuff']];
 
+//function
+
+function hello_world() {
+   alert ('Hello World');
+}
+
+hello_world();
