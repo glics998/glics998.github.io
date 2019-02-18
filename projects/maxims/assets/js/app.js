7dae88b35a9a0182cf5c55b40c975101affd3031
sierra <owner@Owners-MBP.fios-router.home>
2019-02-18 11:37:08 -0500

    neon dem fin

---
+++ projects/maxims/assets/js/app.js
@@ -0,0 +1,23 @@
+const signs = document.querySelectorAll('x-sign')
+const randomIn = (min, max) => (
+  Math.floor(Math.random() * (max - min + 1) + min)
+)
+
+const mixupInterval = el => {
+  const ms = randomIn(2000, 4000)
+  el.style.setProperty('--interval', `${ms}ms`)
+}
+
+signs.forEach(el => {
+  mixupInterval(el)
+  el.addEventListener('webkitAnimationIteration', () => {
+    mixupInterval(el)
+  })
+})
+
+@keyframes flicker {
+    50% {
+      color: white;
+      filter: saturate(200%) hue-rotate(20deg);
+    }
+  }
