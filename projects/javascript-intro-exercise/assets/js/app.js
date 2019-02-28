138fea6f774f3da60fb85f94ff0079ed6bf9b762
sierra <owner@Owners-MacBook-Pro.local>
2019-02-27 22:20:00 -0500

    java fin

---
+++ projects/javascript-intro-exercise/assets/js/app.js
@@ -0,0 +1,108 @@
+var myName= 'Sierra';
+console.log ('My name is', myName);
+
+var favNum= 28;
+console.log ('My favorite number is', favNum);
+
+var favNumString = '28';
+console.log ('My favorite number is', favNumString);
+
+var boolean = false;
+console.log (boolean);
+
+var arrayName = ['s', 'i', 'e', 'r', 'r', 'a'];
+console.log (arrayName);
+
+var arrayData = ['Sierra','28', 'Lab' ];
+console.log (arrayData);
+
+var fullName = ['Sierra', 'Glicker' ];
+console.log (fullName);
+
+var equalHundred = 50+ 50;
+console.log (equalHundred);
+
+console.log (arrayName, [7]);
+console.log (arrayName,length);
+
+console.log ('supercalifragilisticexpialidocious', length);
+
+console.log (fullName [13],length);
+console.log (fullName [13], length);
+
+if ('All strudents are here') {
+  console.log ('All students are in class')
+} else {
+    console.log ('Not everyone is here')
+};
+
+for (var i= 0; i < arrayName, length; i++){
+arrayName [i]
+console.log (looparrayName [i]);
+};
+
+var date= new Date;
+var hour= date.getHours();
+var minutes= date.getMinutes();
+console.log (hour, minutes);
+
+var today= Date;
+console.log ('Today is',date);
+
+var date = new Date;
+var hour = date.getHours();
+
+if ( hour >= 19 && hour < 22) {
+console.log('Its between 7pm and 9:40pm')
+};
+
+var arrayFood= ['pasta', 'ice cream', 'mac n cheese'];
+console.log (arrayFood);
+
+var arrayAnimal= ['dog', 'elephant', 'whale'];
+console.log (arrayAnimal);
+
+var arrayPlaces= ['New York', 'Paris', 'London'];
+console.log (arrayPlaces);
+
+var arrayPokemon= ['pikachu','vulpix', 'squirtle'];
+console.log (arrayPokemon);
+
+var qten= ['pasta', 'ice cream', 'mac n cheese','dog', 'elephant', 'whale','New York', 'Paris', 'London','pikachu','vulpix', 'squirtle'];
+console.log (qten); 
+
+for ( var i = 0; i < arrayFood, length; i++ ) {
+        console.log( arrayFood[i] + ' is in arrays ' + i );
+      }
+
+ for ( var i = 0; i < arrayAnimal, length; i++ ) {
+        console.log( arrayAnimal[i] + ' is in arrays ' + i );
+      }
+
+ for ( var i = 0; i < arrayPlaces, length; i++ ) {
+        console.log( arrayPlaces[i] + ' is in arrays ' + i );
+      }
+
+for ( var i = 0; i < arrayPokemon, length; i++ ) {
+        console.log( arrayPokemon[i] + ' is in arrays ' + i );
+      }
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
