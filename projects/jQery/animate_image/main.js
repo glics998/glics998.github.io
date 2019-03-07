97f9f504b8d36c9b1c0765092d50d1949297afdc
sierra <owner@owners-mbp.wireless.newschool.edu>
2019-03-06 21:01:27 -0500

    edit animate

--- projects/jQery/animate_image/main.js
+++ projects/jQery/animate_image/main.js
@@ -1,3 +1,10 @@
-$(function() {
-  // Your interactions go here
+ 
+$( "#seagull" ).click(function() {
+  $( "#seagull" ).animate({
+    
+    marginLeft: "12in",
+  borderWidth: "10px"
+  }, 1500 );
 });
+
+
