ec91f1ede5d0f5111b427a04fbb5e56dcfb6ff67
sierra <owner@Owners-MacBook-Pro.local>
2019-03-08 16:37:08 -0500

    fin click

--- projects/jQery/click_events/main.js
+++ projects/jQery/click_events/main.js
@@ -1,3 +1,23 @@
-$(function() {
-  // Your interactions go here
+
+  $(document).ready(function(){
+    $("#div1").click(function(){
+    $("#chgcolor").css("background-color","rgb(221, 18, 86)");
+    });
+    
+    $("#div2").click(function(){
+    $("#chgcolor").css("background-color","rgb(233, 71, 125)");
+    });
+    
+    $("#div3").click(function(){
+    $("#chgcolor").css("background-color","rgb(250, 88, 162)");
+    });
+    
+    $("#div4").click(function(){
+    $("#chgcolor").css("background-color","rgb(250, 126, 167)");
+    });
+    
+    $("#div5").click(function(){
+    $("#chgcolor").css("background-color","rgb(250, 156, 186)");
+    });
 });
+
