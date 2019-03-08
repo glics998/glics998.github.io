b4e269e459d13f09121ffbd3c287962a0447b54f
sierra <owner@owners-mbp.wireless.newschool.edu>
2019-03-08 12:21:44 -0500

    edits

--- projects/jQery/animate_image/main.js
+++ projects/jQery/animate_image/main.js
@@ -1,10 +1,11 @@
- 
+$(function() {
 $( "#seagull" ).click(function() {
   $( "#seagull" ).animate({
-    
-    marginLeft: "12in",
+  marginLeft: "12in",
   borderWidth: "10px"
   }, 1500 );
 });
+});
+
 
 
--- projects/jQery/random_boxes/main.js
+++ projects/jQery/random_boxes/main.js
@@ -1,3 +1,11 @@
 $(function() {
-  // Your interactions go here
+	$('#button').click(function(){
+		var number = (Math.floor(Math.random() * 5) + 1);
+	
+
+	$('#box').removeClass('#color');
+	$('#box').eq(number).addClass('#color');
+	console.log("number")
+	
+	});
 });
