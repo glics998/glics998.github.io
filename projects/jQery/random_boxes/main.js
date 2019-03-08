ab2f017f7c5c43a48aee33cc47dfbdfa0d0ad9f5
sierra <owner@owners-mbp.wireless.newschool.edu>
2019-03-08 13:12:00 -0500

    scroll events

--- projects/jQery/random_boxes/main.js
+++ projects/jQery/random_boxes/main.js
@@ -1,11 +1,1 @@
-$(function() {
-	$('.button').click(function() {
-	console.log('i clicked it');
-	var number = (Math.floor(Math.random() * 5) + 1);
-	
-	$('.box').removeClass('.color');
-	$('.box').eq(number).addClass('.color');
-	console.log("number");
 
-	  });
-	});
