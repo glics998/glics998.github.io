957c4aedd8b5a6a9b86f7d3c39ff1ff93f850f19
sierra <owner@owners-mbp.wireless.newschool.edu>
2019-03-27 20:23:18 -0400

    clock edit

--- projects/clock/main.js
+++ projects/clock/main.js
@@ -1,15 +1,21 @@
-$(document).ready(function() {
-
-  $(function() {
-      setInterval(function() {
-        var color = 0;
+function refreshData()
+{
+    x = 1;  
+ 	var d = new Date()
+ 	var h = d.getHours();
+ 	var m = d.getMinutes();
+ 	var s = d.getSeconds();
+ 	
+ 	if (h<=9) {h = '0'+h};
+ 	if (m<=9) {m = '0'+m};
+	if (s<=9) {s = '0'+s};
+	
+ 	var	color = '#'+h+m+s;
+ 	
+    $("div.background").css("background-color", color );
+    $("p#hex").text(color);
      
-        var minute = new Date().getMinutes();
-        var hours = new Date().getHours();
-
-        var whatToPrint = hours + ":" + minute + ":";
-        $("#t1").html(whatToPrint);
-      }, 1000);
-    });
-  });
-
+    setTimeout(refreshData, x*1000);
+}
+  
+refreshData();
