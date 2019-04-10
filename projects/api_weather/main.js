cb8f49fb600e17ce37f7e4c9e912937a0778b51b
sierra <owner@owners-mbp.wireless.newschool.edu>
2019-04-10 18:52:48 -0400

    add api

---
+++ projects/api_weather/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.box').click(function() {
+        console.log("hello")
+      
+        var number=Math.floor((Math.random()
+       *5) +0);
+       console.log(number)
+      
+      $('.one').removeClass('active');
+      $('.one').eq(number).addClass('active');
+   
+  });
+});
+ 
